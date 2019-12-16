cc7e248f077c969238be9452d1197c805e66ab63
ric3o <julenricocrespo@gmail.com>
2019-12-16 23:39:44 +0100

    FAB ADDED

--- app/src/main/java/com/example/orientacionescolar/activities/InfoConsultingActivityVd.java
+++ app/src/main/java/com/example/orientacionescolar/activities/InfoConsultingActivityVd.java
@@ -1,5 +1,6 @@
 package com.example.orientacionescolar.activities;
 
+import android.animation.Animator;
 import android.os.Bundle;
 
 import com.example.orientacionescolar.R;
@@ -11,11 +12,16 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.orientacionescolar.activities.ui.main.FragmentsAdapter;
 
 public class InfoConsultingActivityVd extends AppCompatActivity {
 
+    FloatingActionButton fab, fab1, fab2, fab3;
+    LinearLayout fabLayout1, fabLayout2, fabLayout3;
+    View fabBGLayout;
+    boolean isFABOpen = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +31,91 @@ public class InfoConsultingActivityVd extends AppCompatActivity {
         viewPager.setAdapter(fragmentsAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.floating_action_button);
+
+        fabLayout1 =  findViewById(R.id.fabLayout1);
+        fabLayout2 =  findViewById(R.id.fabLayout2);
+        fabLayout3 =  findViewById(R.id.fabLayout3);
+        fab =  findViewById(R.id.fab);
+        fab1 =  findViewById(R.id.fab1);
+        fab2 =  findViewById(R.id.fab2);
+        fab3 =  findViewById(R.id.fab3);
+        fabBGLayout = findViewById(R.id.fabBGLayout);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!isFABOpen) {
+                    showFABMenu();
+                } else {
+                    closeFABMenu();
+                }
+            }
+        });
+
+        fabBGLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeFABMenu();
+            }
+        });
+    }
+
+    private void showFABMenu() {
+        isFABOpen = true;
+        fabLayout1.setVisibility(View.VISIBLE);
+        fabLayout2.setVisibility(View.VISIBLE);
+        fabLayout3.setVisibility(View.VISIBLE);
+        fabBGLayout.setVisibility(View.VISIBLE);
+        fab.animate().rotationBy(180);
+        fabLayout1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        fabLayout2.animate().translationY(-getResources().getDimension(R.dimen.standard_100));
+        fabLayout3.animate().translationY(-getResources().getDimension(R.dimen.standard_145));
+    }
+
+    private void closeFABMenu() {
+        isFABOpen = false;
+        fabBGLayout.setVisibility(View.GONE);
+        fab.animate().rotation(0);
+        fabLayout1.animate().translationY(0);
+        fabLayout2.animate().translationY(0);
+        fabLayout3.animate().translationY(0);
+        fabLayout3.animate().translationY(0).setListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                if (!isFABOpen) {
+                    fabLayout1.setVisibility(View.GONE);
+                    fabLayout2.setVisibility(View.GONE);
+                    fabLayout3.setVisibility(View.GONE);
+                }
+/*                if (fab.getRotation() != -180) {
+                    fab.setRotation(-180);
+                }*/
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
 
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isFABOpen) {
+            closeFABMenu();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
